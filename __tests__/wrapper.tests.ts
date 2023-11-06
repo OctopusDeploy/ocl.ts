6d8779dd4917cf9743bb18341a916280c7f68232
Matthew Casperson <matthewcasperson@gmail.com>
2023-11-07 07:15:06 +1000

    Wrapped up the root node to make allow it to be queried. Passed through array gets to allow collections to work as normal.

--- __tests__/wrapper.tests.ts
+++ __tests__/wrapper.tests.ts
@@ -1,7 +1,7 @@
-import {parseSteps} from "../src/wrapper";
+import {parseOclWrapper} from "../src/wrapper";
 
 test("Wrapper OCL property access", () => {
-    const wrapper = parseSteps(`step "back-up-store-client-filesystem" {
+    const wrapper = parseOclWrapper(`step "back-up-store-client-filesystem" {
     name = "Upgrade POS client software"
     number_value = 10
     bool_value = false
@@ -83,20 +83,146 @@ test("Wrapper OCL property access", () => {
             }
         }
     }
+}
+
+step "back-up-store-server-filesystem" {
+    name = "Back up store server filesystem"
+    properties = {
+        Octopus.Action.TargetRoles = "pos-server"
+    }
+
+    action {
+        action_type = "Octopus.Script"
+        properties = {
+            Octopus.Action.RunOnServer = "false"
+            Octopus.Action.Script.ScriptBody = <<-EOT
+                Write-Highlight "Backing up store server filesystem"
+                
+                Start-Sleep 5
+                
+                Write-Highlight "Finished backing up store server filesystem"
+                    EOT
+            Octopus.Action.Script.ScriptSource = "Inline"
+            Octopus.Action.Script.Syntax = "PowerShell"
+        }
+    }
+}
+
+step "upgrade-store-server-database" {
+    name = "Upgrade store server database"
+    properties = {
+        Octopus.Action.TargetRoles = "pos-server"
+    }
+
+    action {
+        action_type = "Octopus.Script"
+        properties = {
+            Octopus.Action.RunOnServer = "false"
+            Octopus.Action.Script.ScriptBody = <<-EOT
+                $connection = $OctopusParameters["Pos.Server.Database.ConnectionString"]
+                $new = $OctopusParameters["Octopus.Release.Number"]
+                
+                Write-Highlight "Upgrading store server database"
+                Write-Highlight "Using connection string: $connection"
+                Write-Highlight "Applying migrations for version $new"
+                
+                Start-Sleep 5
+                
+                Write-Highlight "Finished upgrading store server database"
+                    EOT
+            Octopus.Action.Script.ScriptSource = "Inline"
+            Octopus.Action.Script.Syntax = "PowerShell"
+        }
+
+        packages "Pos.Server.Migrations" {
+            acquisition_location = "Server"
+            feed = "octopus-server-built-in"
+            package_id = "Pos.Server.Migrations"
+            properties = {
+                Extract = "False"
+                Purpose = ""
+                SelectionMode = "immediate"
+            }
+        }
+    }
+}
+
+step "upgrade-store-server-software" {
+    name = "Upgrade store server software"
+    properties = {
+        Octopus.Action.TargetRoles = "pos-server"
+    }
+
+    action {
+        action_type = "Octopus.Script"
+        properties = {
+            Octopus.Action.RunOnServer = "false"
+            Octopus.Action.Script.ScriptBody = <<-EOT
+                $current = $OctopusParameters["Octopus.Release.Previous.Number"]
+                $new = $OctopusParameters["Octopus.Release.Number"]
+                
+                Write-Highlight "Upgrading store server software"
+                Write-Highlight "Current version is $current"
+                Write-Highlight "New version is $new"
+                
+                Start-Sleep 5
+                
+                Write-Highlight "Finished upgrading store server software"
+                    EOT
+            Octopus.Action.Script.ScriptSource = "Inline"
+            Octopus.Action.Script.Syntax = "PowerShell"
+        }
+
+        packages "Pos.Server.Application" {
+            acquisition_location = "Server"
+            feed = "octopus-server-built-in"
+            package_id = "Pos.Server.Application"
+            properties = {
+                Extract = "False"
+                Purpose = ""
+                SelectionMode = "immediate"
+            }
+        }
+    }
 }`)
 
-    expect(wrapper[0].action['upgrade-store-client-software'].properties['Octopus.Action.RunOnServer']).toEqual("false")
-    expect(wrapper[0].action['upgrade-store-client-software'].__name).toEqual("action")
-    expect(wrapper[0].action['upgrade-store-client-software'].packages['Pos.Client.Application'][0].properties[1].Purpose).toEqual("Second properties")
-    expect(wrapper[0].action['upgrade-store-client-software'].packages['Pos.Client.Application'][0].properties[0].Purpose).toEqual("")
-    expect(wrapper[0].action['upgrade-store-client-software'].packages['Pos.Client.Application'][1].properties[1].Purpose).toEqual("Third properties")
-    expect(wrapper[0].action['upgrade-store-client-software'].packages['Pos.Client.Application'][1].properties[0].Purpose).toEqual("")
-    expect(wrapper[0].name).toEqual("Upgrade POS client software")
-    expect(wrapper[0].number_value).toEqual(10)
-    expect(wrapper[0].bool_value).toBeFalsy()
-    expect(wrapper[0].properties['Octopus.Action.MaxParallelism']).toEqual("100")
-    expect(wrapper[0].action['back-up-store-client-filesystem'].action_type).toEqual("Octopus.Script")
-    expect(wrapper[0].__labels[0]).toEqual("back-up-store-client-filesystem")
-    expect(wrapper[0].__labels.length).toEqual(1)
-    expect(wrapper[0].__name).toEqual("step")
+    // wrapper is read only
+    wrapper.test = 'test'
+    expect(wrapper.test).toBeUndefined()
+
+    // The root node exposes blocks as an array
+    expect(wrapper.step.length).toEqual(4)
+
+    // Here we access the first step
+    expect(wrapper.step[0].action.length).toEqual(2)
+    expect(wrapper.step[1].action.length).toEqual(1)
+
+    // Nodes are also accessible via labels. Here we access the step via its label
+    expect(wrapper.step["back-up-store-client-filesystem"].action.length).toEqual(2)
+    expect(wrapper.step["back-up-store-server-filesystem"].action.length).toEqual(1)
+
+    // Missing labels are undefined
+    expect(wrapper.step["does-not-exist"]).toBeUndefined()
+
+    // Some special properties to access a block's name and labels
+    expect(wrapper.step["back-up-store-client-filesystem"].__labels[0]).toEqual("back-up-store-client-filesystem")
+    expect(wrapper.step["back-up-store-client-filesystem"].__labels.length).toEqual(1)
+    expect(wrapper.step["back-up-store-client-filesystem"].__name).toEqual("step")
+
+    // More tests that drill deeper into the structure
+    expect(wrapper.step["back-up-store-client-filesystem"].action['upgrade-store-client-software'].packages['Pos.Client.Application'][0].properties[0].Purpose).toEqual("")
+    expect(wrapper.step["back-up-store-client-filesystem"].action['upgrade-store-client-software'].packages['does-not-exist']).toBeUndefined()
+    expect(wrapper.step["back-up-store-client-filesystem"].action['upgrade-store-client-software'].packages['Pos.Client.Application'][0].properties[1].Purpose).toEqual("Second properties")
+    expect(wrapper.step["back-up-store-client-filesystem"].action['upgrade-store-client-software'].packages['Pos.Client.Application'][1].properties[1].Purpose).toEqual("Third properties")
+    expect(wrapper.step["back-up-store-client-filesystem"].action['upgrade-store-client-software'].packages['Pos.Client.Application'][1].properties[0].Purpose).toEqual("")
+    expect(wrapper.step["back-up-store-client-filesystem"].action['upgrade-store-client-software'].properties['Octopus.Action.RunOnServer']).toEqual("false")
+    expect(wrapper.step["back-up-store-client-filesystem"].action[0].properties['Octopus.Action.RunOnServer']).toEqual("false")
+    expect(wrapper.step["back-up-store-client-filesystem"].action['upgrade-store-client-software'].__name).toEqual("action")
+    expect(wrapper.step["back-up-store-client-filesystem"].name).toEqual("Upgrade POS client software")
+    expect(wrapper.step["back-up-store-client-filesystem"].number_value).toEqual(10)
+    expect(wrapper.step["back-up-store-client-filesystem"].bool_value).toBeFalsy()
+    expect(wrapper.step["back-up-store-client-filesystem"].properties['Octopus.Action.MaxParallelism']).toEqual("100")
+    expect(wrapper.step["back-up-store-client-filesystem"].action['back-up-store-client-filesystem'].action_type).toEqual("Octopus.Script")
+    expect(wrapper.step["back-up-store-client-filesystem"].action[1].action_type).toEqual("Octopus.Script")
+
 })
--- src/wrapper.ts
+++ src/wrapper.ts
@@ -1,6 +1,6 @@
 import {Lexer} from "./lexer";
 import {Parser} from "./parser";
-import type {ASTNode, AttributeNode, BlockNode, DictionaryNode, LiteralNode} from "./ast";
+import type {AST, ASTNode, AttributeNode, BlockNode, DictionaryNode, LiteralNode} from "./ast";
 import {NodeType} from "./ast";
 import type {Token} from "./token";
 
@@ -10,25 +10,46 @@ import type {Token} from "./token";
  * regular JavaScript objects.
  * @param input The OCL to parse
  */
-export function parseSteps(input: string) {
+export function parseOclWrapper(input: string): any {
     const lexer = new Lexer(input)
     const parser = new Parser(lexer)
     const ast = parser.getAST()
-    return ast.map(step => new Proxy(step, {
-        get: function (target, name) {
-            return getProperty(target, name.toString())
+
+    /*
+        The top level object is treated as a "root node" rather than a plain array. It responds to
+        any requests to an index or properties like length, but also allows blocks and attributes to
+        be returned by name.
+     */
+    return new Proxy(ast, {
+            get: function (target, name): any {
+                // return any array based properties as normal
+                if (name in target) {
+                    return wrapItem(target[name as any]);
+                }
+
+                const attributes = wrapChildAttributes(target, name.toString())
+                if (attributes != undefined) {
+                    return attributes
+                }
+
+                return wrapChildArray(target, name.toString())
+            },
+            set: function () {
+                // no op - this is a read only object
+                return true
+            }
         }
-    })) as any
+    )
 }
 
-function getProperty(node: ASTNode | undefined, name: string): string | number | boolean | DictionaryNode | (string | number | boolean | DictionaryNode | undefined)[] | BlockNode[] | undefined {
+function getProperty(node: ASTNode | undefined, name: string): any {
     if (!node || !name) {
         return undefined
     }
 
     // __labels and __name are special property that returns the labels assigned to the block
     // and tyhe name of the block
-    if (node.type === NodeType.BLOCK_NODE ) {
+    if (node.type === NodeType.BLOCK_NODE) {
         if (name === "__labels") {
             return node.labels?.map(l => JSON.parse(l.value.value))
         }
@@ -43,17 +64,9 @@ function getProperty(node: ASTNode | undefined, name: string): string | number |
     // Otherwise we try to find the children with the supplied name
     if (node.type === NodeType.BLOCK_NODE || node.type == NodeType.DICTIONARY_NODE) {
         // find attribute nodes with the name and return the raw value
-        const attributeChild = node.children
-            ?.filter(c =>
-                c.type === NodeType.ATTRIBUTE_NODE &&
-                c.name.value === name)
-            .map(c => getUnquotedPropertyValue(c as AttributeNode))
-
-        if (attributeChild !== undefined && attributeChild.length != 0) {
-            if (attributeChild.length === 1) {
-                return attributeChild.pop()
-            }
-            return attributeChild
+        const attributes = wrapChildAttributes(node.children, name)
+        if (attributes != undefined) {
+            return attributes
         }
 
         // find block nodes with the name and wrap it up in a proxy
@@ -63,11 +76,22 @@ function getProperty(node: ASTNode | undefined, name: string): string | number |
                 c.name.value === name)
             .map(c => c as BlockNode)
 
-        if (blockChildren) {
+        if (blockChildren && blockChildren.length != 0) {
             // An array of block children is distinguished by their first label, which is
             // selected as if it were a property.
             return new Proxy(blockChildren, {
+                set: function () {
+                    // no op - this is a read only object
+                    return true
+                },
                 get: function (target, name) {
+
+                    // return any array based properties as normal
+                    if (name in target) {
+                        return wrapItem(target[name as any]);
+                    }
+
+                    // Otherwise, look up the child based on a label
                     const children = target.filter(b => b.labels
                         ?.map(l => JSON.parse(l.value.value))
                         .pop() === name)
@@ -85,7 +109,8 @@ function getProperty(node: ASTNode | undefined, name: string): string | number |
                                 )
                             }
                         } else if (children.length > 1) {
-                            // If there are multiple children, return then all as an array
+                            // If there are multiple children (for example, there are blocks with the same label), return
+                            // an array of children
                             return children.map(c => new Proxy(c, {
                                     get: function (target, name) {
                                         return getProperty(target, name.toString())
@@ -118,8 +143,112 @@ function getUnquotedPropertyValue(node: AttributeNode | undefined): string | num
 
     if (node.value.type === NodeType.DICTIONARY_NODE) {
         return new Proxy(node.value, {
+            set: function () {
+                // no op - this is a read only object
+                return true
+            },
             get: function (target, name) {
                 return getProperty(target, name.toString())
+            }
+        })
+    }
+
+    return undefined
+}
+
+function wrapChildAttributes(target: AST, name: string) {
+    const attributes = target
+        ?.filter(c =>
+            c.type === NodeType.ATTRIBUTE_NODE &&
+            c.name.value === name)
+        .map(c => getUnquotedPropertyValue(c as AttributeNode))
+
+    if (attributes !== undefined && attributes.length != 0) {
+        if (attributes.length === 1) {
+            return attributes.pop()
+        }
+        return attributes
+    }
+
+    return undefined
+}
+
+/**
+ * Takes a value returned from an array, which could have been an indexed lookup of an object, or could have
+ * been a request for the array length, and returns the appropriate value.
+ * @param item The property from the array
+ */
+function wrapItem(item: any): any {
+    // assume an object being returned was an index lookup
+    if (typeof item === 'object') {
+        // this has to be proxied
+        return new Proxy(item, {
+            set: function () {
+                // no op - this is a read only object
+                return true
+            },
+            get: function (target, name) {
+                return getProperty(target, name.toString())
+            }
+        })
+    }
+
+    // anything else is assumed to be a lookup like "length"
+    return item
+}
+
+function wrapChildArray(target: AST, name: string) {
+    const children = target
+        .filter(c => c.type == NodeType.BLOCK_NODE)
+        .map(c => c as BlockNode)
+        .filter(c => c.name.value === name)
+
+    if (children && children.length != 0) {
+        return new Proxy(children, {
+            set: function () {
+                // no op - this is a read only object
+                return true
+            },
+            get: function (target, name) {
+                // return any array based properties as normal
+                if (name in target) {
+                    return wrapItem(target[name as any]);
+                }
+
+                // Otherwise, look up the child based on a label
+                const children = target.filter(b => b.labels
+                    ?.map(l => JSON.parse(l.value.value))
+                    .pop() === name)
+
+                if (children.length === 1) {
+                    const child = children.pop()
+                    if (child) {
+                        return new Proxy(child, {
+                            set: function () {
+                                // no op - this is a read only object
+                                return true
+                            },
+                            get: function (target, name) {
+                                return getProperty(target, name.toString())
+                            }
+                        })
+                    }
+                }
+
+                if (children.length > 1) {
+
+                    return children.map(c => new Proxy(c, {
+                        set: function () {
+                            // no op - this is a read only object
+                            return true
+                        },
+                        get: function (target, name) {
+                            return getProperty(target, name.toString())
+                        }
+                    }))
+                }
+
+                return undefined
             }
         })
     }
